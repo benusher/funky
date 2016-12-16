4c63966c15f4a4ea14318a2ae1755f20e9a1ca06
Ben Usher <ben_usher@hotmail.com>
2016-12-16 13:54:38 +0000

    FlatMap a function over each element of a non-empty list

--- src/main/kotlin/ponchonog/funky/NonEmptyList.kt
+++ src/main/kotlin/ponchonog/funky/NonEmptyList.kt
@@ -9,6 +9,20 @@ data class NonEmptyList<out T>(val head: T, val tail: List<T>) {
     fun <U> map(f: (T) -> U): NonEmptyList<U> =
         nel(f(head), tail.map(f))
 
+    fun <B> flatMap(f: (T) -> NonEmptyList<B>): NonEmptyList<B> {
+        val b = mutableListOf<B>()
+        val p = f(head)
+        b += p.head
+        b += p.tail
+        tail.forEach { a ->
+            val p2 = f(a)
+            b += p2.head
+            b += p2.tail
+        }
+        val bb = b.toList()
+        return nel(bb.head, bb.tail)
+    }
+
     companion object {
 
         fun <T> nel(head: T, vararg tail: T) =
@@ -18,3 +32,9 @@ data class NonEmptyList<out T>(val head: T, val tail: List<T>) {
             NonEmptyList(head, tail)
     }
 }
+
+val <T> List<T>.tail: List<T>
+    get() = drop(1)
+
+val <T> List<T>.head: T
+    get() = first()
--- src/test/kotlin/ponchonog/funky/NonEmptyListTest.kt
+++ src/test/kotlin/ponchonog/funky/NonEmptyListTest.kt
@@ -8,12 +8,17 @@ import ponchonog.funky.NonEmptyList.Companion.nel
 class NonEmptyListTest {
 
     @Test
-    fun `to List`() {
+    fun `To List`() {
         assertThat(nel("a", "b").asList(), equalTo(listOf("a", "b")))
     }
 
     @Test
     fun `Map a function over the list`() {
         assertThat(nel(1, 2).map{it + 1}, equalTo(nel(2, 3)))
+    }
+
+    @Test
+    fun `FlatMap a function over the list`() {
+        assertThat(nel(1, 2).flatMap{nel(it+1)}, equalTo(nel(2, 3)))
     }
 }
